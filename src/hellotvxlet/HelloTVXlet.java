a3ec56e55d16864398d3aeaac6a16725a30932a2
Angelo <angelo.sanada@gmail.com>
2018-06-18 20:02:40 +0200

    basis scene gemaakt met achtergronden

--- src/hellotvxlet/HelloTVXlet.java
+++ src/hellotvxlet/HelloTVXlet.java
@@ -1,29 +1,54 @@
 package hellotvxlet;
 
-import javax.tv.xlet.*;
-
+import java.awt.Color;
+import javax.tv.xlet.Xlet;
+import javax.tv.xlet.XletContext;
+import javax.tv.xlet.XletStateChangeException;
+import org.havi.ui.HScene;
+import org.havi.ui.HSceneFactory;
+import org.havi.ui.HStaticText;
+import org.havi.ui.HTextButton;
+import org.havi.ui.HVisible;
 
 public class HelloTVXlet implements Xlet {
 
-  
-    public HelloTVXlet() {
-        
-    }
-
-    public void initXlet(XletContext context) {
-      
+    public void destroyXlet(boolean unconditional) throws XletStateChangeException {
      
     }
 
-    public void startXlet() {
-    
+    public void initXlet(XletContext ctx) throws XletStateChangeException {
+        HScene scene=HSceneFactory.getInstance().getDefaultHScene();
+        // SCHERM = 720 x 576
+        
+        HStaticText hst0=new HStaticText("Dit is een basis scene",0,100,720,376); // tekst,x,y,w,h
+        HStaticText hst1=new HStaticText("Dit is een basis scene",0,476,720,100); // tekst,x,y,w,h
+        HStaticText hst2=new HStaticText("Dit is een basis scene",0,0,720,100); // tekst,x,y,w,
+        
+        hst0.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        hst0.setBackground(Color.GREEN);
+        
+        hst1.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        hst1.setBackground(Color.BLACK);
+        
+        hst2.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        hst2.setBackground(Color.BLACK);
+        
+        scene.add(hst0);
+        scene.add(hst1);
+        scene.add(hst2);
+
+        scene.validate();
+        scene.setVisible(true);
     }
 
     public void pauseXlet() {
+       
+    }
+
+    public void startXlet() throws XletStateChangeException {
      
     }
 
-    public void destroyXlet(boolean unconditional) {
-     
-    }
+
+  
 }
