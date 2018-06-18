389cd5c3b9896b5a8645e30b7d3a5d5dc6f88d79
Angelo <angelo.sanada@gmail.com>
2018-06-19 00:53:06 +0200

    Draw pong players visually on the main screen

--- src/hellotvxlet/HelloTVXlet.java
+++ src/hellotvxlet/HelloTVXlet.java
@@ -1,6 +1,9 @@
 package hellotvxlet;
 
 import java.awt.Color;
+import java.awt.Container;
+import java.awt.Graphics;
+import java.awt.BorderLayout;
 import javax.tv.xlet.Xlet;
 import javax.tv.xlet.XletContext;
 import javax.tv.xlet.XletStateChangeException;
@@ -11,43 +14,67 @@ import org.havi.ui.HVisible;
 
 public class HelloTVXlet implements Xlet {
 
+    private int sceneWidth = 720;
+            
+    private int sceneHeight = 576;
+    
     public Player player1;
     public Player player2;
+    
+    private HScene scene;
+    
+    private Container gui;
     
     public void destroyXlet(boolean unconditional) throws XletStateChangeException {
      
     }
 
     public void initXlet(XletContext ctx) throws XletStateChangeException {
-        HScene scene=HSceneFactory.getInstance().getDefaultHScene();
+        scene=HSceneFactory.getInstance().getDefaultHScene();
+        
+        gui = new Container(){
+            public void paint(Graphics graphics){
+                
+                graphics.setColor(Color.BLACK);
+                
+                graphics.fillRect(0, 0, sceneWidth, sceneHeight);
+
+                player1.paint(graphics);
+                player2.paint(graphics);
+            }
+        };
+        
+        scene.add(gui, BorderLayout.CENTER);
+        scene.validate();
+        
+        gui.setSize(sceneWidth, sceneHeight);
+        
+     
+        player1 = new Player(50, sceneHeight/2);
+        player2 = new Player(sceneWidth - 50, sceneHeight/2);
+        
         // SCHERM = 720 x 576
         
-        HStaticText hst0=new HStaticText("Dit is een basis scene",0,100,720,376); // tekst,x,y,w,h
-        HStaticText hst1=new HStaticText("Dit is een basis scene",0,476,720,100); // tekst,x,y,w,h
-        HStaticText hst2=new HStaticText("Dit is een basis scene",0,0,720,100); // tekst,x,y,w,
+        //HStaticText hst0=new HStaticText("Dit is een basis scene",0,100,720,376); // tekst,x,y,w,h
+        //HStaticText hst1=new HStaticText("Dit is een basis scene",0,476,720,100); // tekst,x,y,w,h
+        //HStaticText hst2=new HStaticText("Dit is een basis scene",0,0,720,100); // tekst,x,y,w,
                
-        hst0.setBackgroundMode(HVisible.BACKGROUND_FILL);
-        hst0.setBackground(Color.GREEN);
+        //hst0.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        //hst0.setBackground(Color.GREEN);
         
-        hst1.setBackgroundMode(HVisible.BACKGROUND_FILL);
-        hst1.setBackground(Color.BLACK);
+        //hst1.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        //hst1.setBackground(Color.BLACK);
         
-        hst2.setBackgroundMode(HVisible.BACKGROUND_FILL);
-        hst2.setBackground(Color.BLACK);
+        //hst2.setBackgroundMode(HVisible.BACKGROUND_FILL);
+        //hst2.setBackground(Color.BLACK);
         
-        scene.add(hst0);
-        scene.add(hst1);
-        scene.add(hst2);
+        //scene.add(hst0);
+        //scene.add(hst1);
+        //scene.add(hst2);
         
-        scene.validate();
-        scene.setVisible(true);
-        
-        player1 = new Player(50, 576/2);
-        player2 = new Player(720 - 50, 576/2);
-        
-        player1.setPosition(576/2);
-        player2.setPosition(576/2); 
-      
+        //scene.validate();
+        //scene.setVisible(true);
+             
     }
 
     public void pauseXlet() {
@@ -55,8 +82,11 @@ public class HelloTVXlet implements Xlet {
     }
 
     public void startXlet() throws XletStateChangeException {
-     
+        gui.setVisible(true);
+        scene.setVisible(true); 
     }
+    
+    
 
 
   
--- src/hellotvxlet/Player.java
+++ src/hellotvxlet/Player.java
@@ -1,6 +1,8 @@
 package hellotvxlet;
 
 import java.awt.Point;
+import java.awt.Graphics;
+import java.awt.Color;
 
 /**
  *
@@ -12,6 +14,11 @@ public class Player {
     
     public Player(int xPos, int yPos){
         pos = new Point(xPos, yPos);
+    }
+    
+    public void paint(Graphics graphics){
+        graphics.setColor(Color.WHITE);
+        graphics.fillRect(pos.x - 25/2, pos.y - 80/2, 25, 80);
     }
         
     public void setPosition(int yPos){
